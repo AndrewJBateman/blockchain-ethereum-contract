083a8c698992ab93583642d0b316859e33619f85
AndrewJBateman <gomezbateman@yahoo.com>
2021-08-05 17:59:10 +0200

    feat: client-side code added + config. file

---
+++ client/app.js
@@ -0,0 +1,105 @@
+App = {
+	contracts: {},
+	init: async () => {
+		await App.loadWeb3();
+		await App.loadAccount();
+		await App.loadContract();
+		await App.renderBalance();
+		await App.renderTasks();
+	},
+	loadWeb3: async () => {
+		if (window.ethereum) {
+			App.web3Provider = window.ethereum;
+			await window.ethereum.request({ method: "eth_requestAccounts" });
+		} else if (web3) {
+			web3 = new Web3(window.web3.currentProvider);
+		} else {
+			console.log("No ethereum browser is installed. Try installing MetaMask");
+		}
+	},
+	loadAccount: async () => {
+		const accounts = await window.ethereum.request({
+			method: "eth_requestAccounts",
+		});
+		App.account = accounts[0];
+	},
+
+  // @truffle/contract functions setProvider(), deployed()
+	loadContract: async () => {
+		try {
+			const res = await fetch("TasksContract.json");
+			const tasksContractJSON = await res.json();
+			App.contracts.TasksContract = TruffleContract(tasksContractJSON);
+			App.contracts.TasksContract.setProvider(App.web3Provider);
+
+			App.tasksContract = await App.contracts.TasksContract.deployed();
+		} catch (error) {
+			console.error(error);
+		}
+	},
+
+  // render account balance as inner text
+	renderBalance: async () => {
+		document.getElementById("account").innerText = App.account;
+	},
+
+  // render tasks
+	renderTasks: async () => {
+		const taskCounter = await App.tasksContract.taskCounter();
+		const taskCounterNumber = taskCounter.toNumber();
+
+		let html = "";
+
+		for (let i = 1; i <= taskCounterNumber; i++) {
+			const task = await App.tasksContract.tasks(i);
+			const taskId = task[0].toNumber();
+			const taskTitle = task[1];
+			const taskDescription = task[2];
+			const taskDone = task[3];
+			const taskCreatedAt = task[4];
+
+			// Creating a task Card
+			let taskElement = `
+        <div class="card bg-light rounded-0 mb-2">
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <span>${taskTitle}</span>
+            <div class="form-check form-switch">
+              <input class="form-check-input" data-id="${taskId}" type="checkbox" onchange="App.toggleDone(this)" ${
+				taskDone === true && "checked"
+			}>
+            </div>
+          </div>
+          <div class="card-body">
+            <span>${taskDescription}</span>
+            <span>${taskDone}</span>
+            <p class="text-muted">Task was created ${new Date(
+							taskCreatedAt * 1000
+						).toLocaleString()}</p>
+            </label>
+          </div>
+        </div>
+      `;
+			html += taskElement;
+		}
+
+		document.querySelector("#tasksList").innerHTML = html;
+	},
+	createTask: async (title, description) => {
+		try {
+			const result = await App.tasksContract.createTask(title, description, {
+				from: App.account,
+			});
+			console.log("result.logs[0].args", result.logs[0].args);
+			window.location.reload();
+		} catch (error) {
+			console.error(error);
+		}
+	},
+	toggleDone: async (element) => {
+		const taskId = element.dataset.id;
+		await App.tasksContract.toggleDone(taskId, {
+			from: App.account,
+		});
+		window.location.reload();
+	},
+};
---
+++ client/ui.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  App.init();
+});
+
+/** Task form */
+const taskForm = document.querySelector("#taskForm");
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = taskForm["title"].value;
+  const description = taskForm["description"].value;
+  App.createTask(title, description);
+});
